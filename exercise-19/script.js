8d1a0c34b22f13042569c97514d45e1c0cd31dec
Arthur Zall <zallarthur@gmail.com>
2024-10-23 19:46:37 -0300

    solutions

---
+++ exercise-19/script.js
@@ -0,0 +1,15 @@
+let nome = "Arthur Oliveira";
+
+let primeiraNota = 10;
+let segundaNota = 10;
+let media = (primeiraNota + segundaNota) / 2;
+
+if (media >= 7) {
+
+    console.log(`O aluno ${nome} obteve um bom aproveitamento ao obter média total ${media}`);
+
+} else {
+
+    console.log(`O aluno ${nome} não obteve um bom aproveitamento ao obter média tota ${media}`);
+
+}
