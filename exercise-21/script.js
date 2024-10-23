123fa5c688831e5093f69ca413e9167fc1bd88a0
Arthur Zall <zallarthur@gmail.com>
2024-10-23 19:52:59 -0300

    solutions

---
+++ exercise-21/script.js
@@ -0,0 +1,12 @@
+let numeroInt = 7;
+
+let verificacao = numeroInt % 2;
+
+
+if (verificacao === 0) {
+    console.log(`O número ${numeroInt} é um número PAR`);
+
+} else {
+
+    console.log(`O número ${numeroInt} é um número ÍMPAR`)
+}
