e8dca26721ec3b1a8422f553d309096761342011
Arthur Zall <zallarthur@gmail.com>
2024-10-23 00:11:18 -0300

    Solution exercise 11

---
+++ exercicio-11/script.js
@@ -0,0 +1,12 @@
+// 11) Desenvolva uma lógica que leia os valores de A, B e C de uma equação do
+// segundo grau e mostre o valor de Delta.
+
+// delta = b^2 - 4ac
+
+let valueA = 3;
+let valueB = 5;
+let valueC = 2;
+
+let delta = (valueB ** 2) - 4 * valueA * valueC;
+
+console.log(`O valor de delta é: ${delta}`);
