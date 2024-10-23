6e2cbaab9345d981ffb436dbed93a0a9a17df683
Arthur Zall <zallarthur@gmail.com>
2024-10-23 20:42:24 -0300

    solutions

---
+++ exercise-26/script.js
@@ -0,0 +1,16 @@
+let primeiroNumero = 100;
+let segundoNumero = 10;
+
+if (primeiroNumero > segundoNumero) {
+
+    console.log('O primeir valor é maior');
+
+} else if (segundoNumero > primeiroNumero) {
+
+    console.log('O segundo valor é maior');
+
+} else {
+
+    console.log('Não existe valor maior, os dois são iguais');
+
+}
