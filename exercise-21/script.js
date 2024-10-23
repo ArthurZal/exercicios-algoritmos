aa095b69c4aab7210a23d59ec666231f19ecdf87
Arthur Zall <zallarthur@gmail.com>
2024-10-23 19:56:42 -0300

    solutions

--- exercise-21/script.js
+++ exercise-21/script.js
@@ -1,12 +1,13 @@
-let numeroInt = 7;
+let ano = 2001;
 
-let verificacao = numeroInt % 2;
-
+let verificacao = ano % 4;
 
 if (verificacao === 0) {
-    console.log(`O número ${numeroInt} é um número PAR`);
+
+    console.log(`${ano} é um ano BISSEXTO`);
 
 } else {
 
-    console.log(`O número ${numeroInt} é um número ÍMPAR`)
+    console.log(`${ano} NÃO é um ano BISSEXTO`);
+
 }
